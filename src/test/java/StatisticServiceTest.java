92a1587aecabdacd791d4b50b00c2008839e64b1
TriksterTripster <1387751@mail.ru>
2022-12-03 18:01:55 +0500

    initial commit

---
+++ src/test/java/StatisticServiceTest.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class StatisticServiceTest {
+
+    @Test
+    public void sumTest() {
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void averageTest() {
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maximumTest() {
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maximum(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void minimumTest() {
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minimum(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lowAverageTest() {
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.lowAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void aboveAverageTest() {
+        StatisticService service = new StatisticService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.aboveAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
